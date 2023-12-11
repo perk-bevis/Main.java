822eaf882de09837a8abb8219cd9be9c36a90dd5
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-12-11 16:58:30 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -22,3 +22,26 @@ public class main {
     System.out.println(x);
   }
 }
+//nhập dữ liệu từ bàn phím
+
+__________
+import java.util.Scanner;
+
+public class main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("nhap vao ho va ten: ");
+    String hoVaTen = sc.nextLine();
+    
+    System.out.println("nhap ma sinh vien: ");
+    long maSinhVien = sc.nextLong();
+    
+    System.out.println("nhap vao diem thi: ");
+    float diemThi = sc.nextFloat();
+    
+    System.out.println("ho va ten: " + hoVaTen);
+    System.out.println("ma sinh vien: "+ maSinhVien);
+    System.out.println("diem thi: " +diemThi);
+  }
+}
