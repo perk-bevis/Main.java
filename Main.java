81432c4b4672adffdf2e639293a9d6d9d49db92f
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-12-12 08:04:23 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -52,3 +52,31 @@ public class main {
     System.out.println("diem thi: " +diemThi);
   }
 }
+//chuyển đổi kiểu dữ liệu trong java
+public class main {
+  public static void main(String[] args){
+    int a = 100;
+    int b = 2;
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+    // ép kiểu ngầm định
+    float c = a;
+    float d = b;
+    System.out.println("c = " + c);
+    System.out.println("d = " + d);
+    // ép kiểu tường minh
+    float e = 9.5f;
+    float f = 3.5f;
+    System.out.println("e = " + e);
+    System.out.println("f = " + f);
+    
+    int g = (int)e;
+    int h = (int)f;
+    System.out.println("g = " + g);
+    System.out.println("h = " + h);
+    
+    // ép kiểu giữa biến nguyên thủy và đối tượng
+    int x = new Integer(32);
+    System.out.println("x = " + x);
+  }
+}
