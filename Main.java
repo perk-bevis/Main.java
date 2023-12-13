6fd1418cd8bc380bb5e081ca98cf5b4d873df819
perk bevis <130304440+perk-bevis@users.noreply.github.com>
2023-12-13 10:12:19 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -95,5 +95,13 @@ public class main {
     b = sc.nextInt();
     int tong = a+b;
     System.out.println(a +" + "+b+" = "+ tong);
+    int hieu = a-b;
+    System.out.println(a +" - "+b+" = "+ hieu);
+    int tich = a*b;
+    System.out.println(a +" * "+b+" = "+ tich);
+    float thuong = (float)a/b;
+    System.out.println(a +" / "+b+" = "+ thuong);
+    int soDu = a%b;
+    System.out.println(a +" % "+b+" = "+ soDu);
   }
 }
